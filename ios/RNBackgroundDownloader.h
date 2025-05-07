12252e0adff1f86f1b5f3f17584c3df59f36c678
Jacob Spizziri <jspizziri@weare5stones.com>
2025-05-07 12:23:42 -0400

    fix ios compile issues for new arch

--- ios/RNBackgroundDownloader.h
+++ ios/RNBackgroundDownloader.h
@@ -13,7 +13,7 @@ typedef void (^CompletionHandler)();
 @end
 
 #ifdef RCT_NEW_ARCH_ENABLED
-@interface RNBackgroundDownloader () <RNBackgroundDownloaderSpec>
+@interface RNBackgroundDownloader () <NativeRNBackgroundDownloaderSpec>
 
 @end
 #endif
